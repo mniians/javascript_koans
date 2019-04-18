e593e77b7a95746167074cf259568cb98a6d6de7
Mr Myers <myersniiansah@gmail.com>
2019-04-18 18:52:41 -0400

    Changed Agent string to boolean

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -79,7 +79,7 @@ describe("About Objects", function () {
     expect("secretary" in meglomaniac).toBe(false);
 
     meglomaniac.secretary = "Agent Smith";
-    expect("secretary" in meglomaniac).toBe('Agent Smith');
+    expect("secretary" in meglomaniac).toBe(true);
     
     delete meglomaniac.henchman;
     expect("henchman" in meglomaniac).toBe(false);
